7b77d037e16d70f2bcadd94d06d42215d41a4662
jayrem1 <jaydb338@gmail.com>
2025-04-03 17:23:19 -0500

    Update buy_and_sell_stock.c

--- buy_and_sell_stock.c
+++ buy_and_sell_stock.c
@@ -1,6 +1,6 @@
 // Beat 100% of runtimes as of 4/3/2025 with 82% of memory usages beat
 // https://assets.leetcode.com/users/images/bba5d76a-174e-4523-9480-65a92d4c0458_1743718868.113725.png
-// Yes I know the image says "Wrong answer" in the bottom right lol, copy and past this code yourself and watch you beat 99.9% of runtimes
+// Yes I know the image says "Wrong answer" in the bottom right lol, copy and paste this code yourself and watch it beat 99.9% of runtimes
 
 int maxProfit(int* prices, int days) 
 {
